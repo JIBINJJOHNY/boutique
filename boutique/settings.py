0e9f34335952df7a1888cd086696f92f0b5c7d1d
JIBINJJOHNY <jibinjjohny11@gmail.com>
2023-11-05 11:09:35 +0100

    Completed confirmation email

--- boutique/settings.py
+++ boutique/settings.py
@@ -171,6 +171,7 @@ STANDARD_DELIVERY_PERCENTAGE = 10
 STRIPE_CURRENCY = 'usd'
 STRIPE_PUBLIC_KEY = os.environ.get('STRIPE_PUBLIC_KEY')
 STRIPE_SECRET_KEY = os.environ.get('STRIPE_SECRET_KEY')
+DEFAULT_FROM_EMAIL = 'boutiqueado@example.com'
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
